fb229d6bb1ac248171ba4405f9f7495843ffed94
chenmeifang <2987642862@qq.com>
2020-03-21 00:18:17 +0800

    补上上一次提交时漏掉的跟登录功能相关的后台文件

--- koa2/router/login.js
+++ koa2/router/login.js
@@ -1,10 +1,24 @@
 const router = require('koa-router')()
 const https = require('https')
 const dao = require('../server/common/baseDao')
+const query = require('../server/db')
 
 const appid = "wx9122a6cd0208fd3f"  //开发者的appid
 const appsecret = "e7fc57c6644aedd7edf5c3aee486a550"   //开发者的appsecret 登入小程序公共平台内查看
 
+// 添加用户
+module.exports = router.post('/newUser', async (ctx, next) =>{
+    console.log('ctx',ctx.request.body)
+    const params = ctx.request.body
+        let res = await query(`insert into user(nick_name,avatar,wx_id,user_new_login,phone_num,student_num,gender,city,province) values('${params.nickName}','${params.avatar}','${params.openId}','${params.userNewLogin}','${params.phoneNum}','${params.studentNum}','${params.gender}','${params.city}','${params.province}');`)
+        console.log('res')
+        console.log(res)
+        ctx.body = {
+            message: '添加用户成功'
+        }    
+})
+
+// 获取openId
 module.exports = router.get('/getOpenId', async(ctx, next) => {
     console.log(ctx)
     console.log("code:", ctx.query.code) // 获取前端参数
@@ -20,12 +34,7 @@ module.exports = router.get('/getOpenId', async(ctx, next) => {
             res.on('end', async () => {
                 try {
                     res = await dao.selectByFields(1, "user", `wx_id="${openId}"`)
-                    if (res.length == 0){
-                        // 如果数据库未查询到该openId对应的user，就往数据库添加一条数据
-                        dao.addRecord('wx_id', `"${openId}"`, 'user')
-                        console.log('res.length:',res.length)
-                    }
-                    ctx.body = res
+                    ctx.body = openId
                     console.log('res',res)
                     resolve()
                 } catch (e) {
